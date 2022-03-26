669077f771a341f5438e58630e10c4674dd79f3a
ppwqdxlte <rexinwangmin@foxmail.com>
2022-03-26 21:34:09 +0800

    添加1个consumer消费者，使用DiscoveryClient和EurekaClient发现其它instances,可以获得其它实例的基本信息，但是无法获取其它的api信息！！！RestTemplate对象，已知别人的api情况下，才能调用其api...但是怎么获得其它provider的api列表呢？？？？？

---
+++ eureka-consumer1/src/main/java/com/example/eurekaconsumer1/HelloController.java
@@ -0,0 +1,74 @@
+package com.example.eurekaconsumer1;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EurekaServiceInstance;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController("/c1/hello")
+public class HelloController {
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+    @Autowired
+    EurekaClient eurekaClient;
+    //调用远程接口
+    RestTemplate restTemplate = new RestTemplate();
+    /*
+    * DiscoveryClient对象，并不能查询其它服务提供者的接口信息。
+    * */
+    @RequestMapping("/discovery")
+    public String discovery(){
+        System.out.println(discoveryClient.getServices().toString());
+        System.out.println(discoveryClient.description());
+        System.out.println(discoveryClient.getInstances("provider1"));
+        System.out.println(discoveryClient.getOrder());
+
+        List<ServiceInstance> provider1 = discoveryClient.getInstances("provider1");
+        for (ServiceInstance item : provider1) {
+            EurekaServiceInstance serviceInstance = (EurekaServiceInstance)item;
+            System.out.println("----------");
+            System.out.println(serviceInstance.getServiceId());
+            System.out.println(serviceInstance.getInstanceId());
+            System.out.println(serviceInstance.getHost());
+            System.out.println(serviceInstance.getMetadata());
+            System.out.println(serviceInstance.getScheme());
+            System.out.println(serviceInstance.getUri());
+            System.out.println(serviceInstance.isSecure());
+            System.out.println(serviceInstance.getInstanceInfo());
+        }
+
+        return "test Spring cloud's DiscoveryClient";
+    }
+    /*
+    * EurekaClient也无法获取其它provider的接口信息
+    * */
+    @RequestMapping("/testEurekaClient")
+    public String testEurekaClient(){
+        List<InstanceInfo> provider2 = eurekaClient.getInstancesById("DESKTOP-4MRD752:provider2:7912");
+        for (InstanceInfo instanceInfo : provider2) {
+            System.out.println(instanceInfo);
+        }
+        return "test EurekaClient";
+    }
+    /*
+    * 手动填入uri,用RestTemplate对象，调用远程api
+    */
+    @RequestMapping("/testRestTemplate")
+    public String testRestTemplate(){
+        List<InstanceInfo> provider2 = eurekaClient.getInstancesByVipAddress("provider2", false);
+        for (InstanceInfo instanceInfo : provider2) {
+            String url = "http://"+instanceInfo.getHostName()+":"+instanceInfo.getPort()
+                    +"/sayHi";
+            System.out.println(restTemplate.getForObject(url, String.class));
+        }
+        return "test RestTemplate";
+    }
+}
