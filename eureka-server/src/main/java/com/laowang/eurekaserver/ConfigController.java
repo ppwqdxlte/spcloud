7d3c995ac665d90bfa9294e350a4e565ea61d34f
ppwqdxlte <rexinwangmin@foxmail.com>
2022-04-12 11:17:55 +0800

    设置eureka-server为config-client，可以获取配置属性，先启动配置中心拉取配置，才启动config-client，顺序不对会报错

---
+++ eureka-server/src/main/java/com/laowang/eurekaserver/ConfigController.java
@@ -0,0 +1,17 @@
+package com.laowang.eurekaserver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigController {
+
+    @Value("${myconfig}")
+    String myconfig;
+
+    @GetMapping("/myconfig")
+    public String getMyconfig(){
+        return myconfig;
+    }
+}
